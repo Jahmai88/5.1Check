4d3e834cea5788d5166c907d2fbf904b80685b35
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 12:50:11 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int myAge = 0;
+
+        if (myAge >= 19);
+        System.out.print("I am 19; let me see if I can drive yet");
+
+        if (myAge >= 16);
+            System.out.println("I'm old enough to have a driver's license!");
+
+        if (myAge <= 16);
+            System.out.println("I'm not old enough yet!");
+    }
+}
